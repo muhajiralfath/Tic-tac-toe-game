504e85b836991237236c3340759e6b7d15a99eb1
muhajiralfath <muhammadmuhajiralfath@gmail.com>
2022-11-22 14:43:38 +0700

    setting switch player & player name

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 let editedPlayer = 0;
+let activePlayer = 0;
 
 const players = [
   {
@@ -15,12 +16,17 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorInputElement = document.getElementById("error-input");
+const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigButtonElement = document.getElementById(
   "cancel-config-button"
 );
+const startGameButtonElement = document.getElementById("start-game-btn");
+// const gameFieldElements = document.querySelectorAll("#game-board li"); ( alternative pilih salah satu)
+const gameBoardElements = document.getElementById("game-board");
 
 editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -29,3 +35,11 @@ cancelConfigButtonElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
 
 formElement.addEventListener("submit", savePlayerConfig);
+
+startGameButtonElement.addEventListener("click", startNewGame);
+
+// for (const gameFieldElement of gameFieldElements) {
+//   gameFieldElement.addEventListener("click", selectGameField);
+// }
+
+gameBoardElements.addEventListener("click", selectGameField);
